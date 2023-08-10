8e2fec02d295af4bee05ef64f31b589f65d0db4a
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-08-10 17:28:50 +0300

    Правки по замечаниям v2

--- source/js/modules/swiper/slider.js
+++ source/js/modules/swiper/slider.js
@@ -17,56 +17,20 @@ export const initSwiper = () => {
     slideClass: 'swiper-slide',
 
     breakpoints: {
-      1320: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
-      1250: {
-        slidesPerView: 4,
-        spaceBetween: 20,
-      },
-      1150: {
-        slidesPerView: 3,
-        spaceBetween: 15,
-      },
-      1000: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      },
-      900: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
-      800: {
-        slidesPerView: 2,
-        spaceBetween: 70,
+      320: {
+        slidesPerView: 1,
       },
       768: {
-        grabCursor: true,
         slidesPerView: 2,
         spaceBetween: 30,
       },
-      700: {
-        slidesPerView: 3,
-        spaceBetween: 10,
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 5,
       },
-      680: {
-        slidesPerView: 2,
-        spaceBetween: 80,
-      },
-      541: {
-        spaceBetween: 30,
-      },
-      520: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      500: {
-        spaceBetween: 10,
-      },
-      499: {
-        slidesPerView: 1,
-        spaceBetween: 100,
+      1360: {
+        slidesPerView: 4,
+        spaceBetween: 40,
       },
     },
   });
